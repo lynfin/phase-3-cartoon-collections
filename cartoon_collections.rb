0189f7ae181a76af9ff0997b019a8a35f8573a19
Lyn <lynfinman@gmail.com>
2022-07-07 09:47:05 -0400

    commit tree e81eec9317c80e10a93c66cf7b98c6184c772464

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,19 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)
   # Your code here
+  dwarves.to_enum.with_index { |val, index| puts "#{index + 1}. #{val}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.map { |call| call.capitalize + '!' }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.select { |call| call.size > 4 }.size > 0
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(foods)
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types = %w[cheddar gouda camembert]
+  found_cheeses = foods & cheese_types
+  foods & cheese_types ? found_cheeses[0] : nil
 end
